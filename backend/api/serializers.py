7748792231f6e8630e48974719a7ce394aa4e42c
yoonii324 <jyoonii324@gmail.com>
2025-05-22 01:11:04 -0400

    creating custom models

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Note
 
 class UserSerializer(serializers.ModelSerializer): # go projects에서 데이터 모델을 json 모델로 바꾸고 json 모델을 데이터 모델로 만드는 그 과정.
     class Meta:
@@ -10,3 +11,9 @@ class UserSerializer(serializers.ModelSerializer): # go projects에서 데이터
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
--- backend/api/views.py
+++ backend/api/views.py
@@ -7,5 +7,5 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all() # so that we don't create the user that already exists
-    serializer_class = User # tells this view what kind of data we need to accept to make a new user
+    serializer_class = UserSerializer # tells this view what kind of data we need to accept to make a new user
     permission_classes = [AllowAny] # who can actually call this
