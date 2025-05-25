13017cde43d92954ba28ca3d4d8409aef969f96f
yoonii324 <jyoonii324@gmail.com>
2025-05-25 01:13:06 -0400

    writing create view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
 
 # Create your views here.
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+            
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all() # so that we don't create the user that already exists
     serializer_class = UserSerializer # tells this view what kind of data we need to accept to make a new user
